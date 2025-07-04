5d10a3a09ce40077bf35e4c556cad3c522885929
czyt <czyt@w.cn>
2025-07-04 13:00:44 +0800

    change CXXFLAGS only for m1 mac

--- face.go
+++ face.go
@@ -1,6 +1,8 @@
 package face
 
 // #cgo CXXFLAGS: -std=c++1z -Wall -O3 -DNDEBUG -march=native
+// #cgo !(darwin && arm64) CXXFLAGS: -march=native
+// #cgo darwin && arm64 CXXFLAGS: -mcpu=apple-m1
 // #cgo LDFLAGS: -ldlib -ljpeg -llapack -lf77blas -lcblas -latlas -lgfortran -lm -lquadmath
 // #include <stdlib.h>
 // #include <stdint.h>
